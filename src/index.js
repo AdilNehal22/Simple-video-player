1cac0b74f309e18d33804d0fa3355cbccfd0a562
AdilNehal22 <adilnehal80@gmail.com>
2021-11-30 21:03:32 +0500

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,57 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './componenets/search_bar';
+import VideoList from './componenets/video_list';
+import VideoDetail from './componenets/video_detail';
+import YTSearch from 'youtube-api-search';
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyDXMy_PYSbTzC3eAaRf6eHC1VMbHE9O74Q';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+
+class App extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+
+        this.videoSearch('surfboards');
+
+        
+    }
+
+    videoSearch(term){
+        YTSearch({key: API_KEY, term: term}, (videos) => {
+            this.setState({ 
+                videos: videos,
+                selectedVideo: videos[0] 
+            });
+        });
+    }
+
+    render(){
+
+        const videoSearch = _.debounce((term)=>{this.videoSearch(term)}, 300)
+        return(
+            <div>
+
+                <SearchBar onSearchTermChange={videoSearch} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList 
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos} />
+            
+            </div>
+        );
+    }
+
+}
+
+//take this component generated html and put it on page(in DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
